8328eab54f796cd5109d001bbc35264b9d1a2887
kge <kge@corexalys.com>
2020-10-13 15:17:49 +0200

    Setup.py: add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
 setup(
-  name="xg",
-  version="0.0.1",
-  packages=['xg'],
-  scripts=["scripts/xg"]
+    name="xg",
+    description="XG, our git wrapper.",
+    version="0.0.1",
+    packages=["xg"],
+    scripts=["scripts/xg"],
+    install_requires=[
+        "wcwidth",
+    ],
 )
