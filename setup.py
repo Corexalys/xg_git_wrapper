c18b1f23487ffea9756c169a30e46654007b77bc
kge <kge@corexalys.com>
2020-10-14 15:45:09 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="xg",
     description="XG, our git wrapper.",
-    version="0.0.1",
+    version="0.0.2",
     packages=["xg"],
     scripts=["scripts/xg"],
     install_requires=[
